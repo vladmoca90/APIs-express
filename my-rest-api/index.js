06156c35ea49f6e651de6f20ccf36cd76c88ef42
Vlad Mocanu <vladmoca90@gmail.com>
2024-08-25 17:18:38 +0100

    Testing

--- my-rest-api/index.js
+++ my-rest-api/index.js
@@ -6,33 +6,6 @@ app.listen(3000, () => {
 });
 
 // GET request
-
 app.get("/msg", (req, res, next) => {
     res.json({ "message": "Hello, World!" });
-});
-
-// POST request
-
-app.use(express.json()); // for parsing application/json
-
-app.post("/msg", (req, res, next) => {
-    const message = req.body.message;
-    res.json({ "receivedMessage": message });
-});
-
-// With classes
-
-class Message {
-    constructor(content) {
-        this.content = content;
-    }
-
-    getContent() {
-        return this.content;
-    }
-}
-
-app.post("/msg", (req, res, next) => { // Refractor the POST to use the class
-    const newMessage = new Message(req.body.message);
-    res.json({ "receivedMessage": newMessage.getContent() });
 });
