12f859f2baec0d5ec8c1ee3bfa0722a468a7a24d
vladmoca90 <vladmoca90@gmail.com>
2024-10-06 00:17:18 +0100

    Formatted the file

--- my-rest-api/server.js
+++ my-rest-api/server.js
@@ -6,15 +6,15 @@ const fs = require('fs'); //require file system object
 const users = "/getUsers";
 
 // Endpoint to Get a list of users
-app.get(users, function(req, res){
-    fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data){
+app.get(users, function (req, res) {
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
         console.log(data);
         res.end(data); // you can also use res.send()
     });
 })
 
 // Create a server to listen at port 8080
-const server = app.listen(8080, function(){
+const server = app.listen(8080, function () {
     const port = server.address().port
 
     console.log(`REST API demo app listening at http://localhost:${port}${users}`)
