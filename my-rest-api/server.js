568a916c75890efaca4002725411fa912292bf14
vladmoca90 <vladmoca90@gmail.com>
2024-11-03 22:44:04 +0000

    Corrected the .js coedes

--- my-rest-api/server.js
+++ my-rest-api/server.js
@@ -1,22 +1,29 @@
-//REST API demo in Node.js
-const express = require("express"); // requre the express framework
+// REST API demo in Node.js
+const express = require("express"); // require the express framework
 const app = express();
-const fs = require("fs"); //require file system object
+const fs = require("fs"); // require file system module
+const path = require("path"); // require path module for handling file paths
 
-const users = "/getUsers";
+const usersEndpoint = "/getUsers";
 
-// Endpoint to Get a list of users
-app.get(users, function (req, res) {
-    fs.readFile(__dirname + "/" + "users.json", "utf8", function (err, data) {
-        console.log(data);
+// Endpoint to get a list of users
+app.get(usersEndpoint, function (req, res) {
+    const filePath = path.join(__dirname, "users.json");
+    
+    fs.readFile(filePath, "utf8", function (err, data) {
+        if (err) {
+            console.error("Error reading users.json:", err);
+            res.status(500).send("Error reading users data.");
+            return;
+        }
         
-        res.end(data); // you can also use res.send()
+        console.log("Data fetched from users.json:", data);
+        res.send(data); // send data as response
     });
-})
+});
 
 // Create a server to listen at port 8080
 const server = app.listen(8080, function () {
-    const port = server.address().port
-
-    console.log(`REST API demo app listening at http://localhost:${port}${users}`)
-})
+    const port = server.address().port;
+    console.log(`REST API demo app listening at http://localhost:${port}${usersEndpoint}`);
+});
--- my-rest-api/users.js
+++ my-rest-api/users.js
@@ -1,28 +1,28 @@
 const data = {
-    user1: {
+    "user1": {
         "id": 1,
         "firstname": "Munonye",
         "lastname": "Kindson",
         "email": "kany@gmail.com"
     },
-    user2: {
+    "user2": {
         "id": 2,
         "firstname": "Imolode",
         "lastname": "Saffron",
         "email": "saffron@gmail.com"
     },
-    user3: {
+    "user3": {
         "id": 3,
         "firstname": "Munonye",
         "lastname": "Othniel",
         "email": "othniel@gmail.com"
     },
-    user4: {
+    "user4": {
         "id": 4,
         "firstname": "Yuba",
         "lastname": "Oleander",
         "email": "oleander@gmail.com"
     }
-}
+};
 
-module.exports = data;
+export default data;
